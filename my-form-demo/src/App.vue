b1edcc22ed98d2acd83cc17298e87ecfe038f55f
yayxs <leaderywl@163.com>
2020-04-23 22:56:51 +0800

    [my-form-demo]手写自己的elementform表单

---
+++ my-form-demo/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Index />
+  </div>
+</template>
+
+<script>
+import Index from "./components/form/Index";
+export default {
+  name: "App",
+  components: {
+    Index,
+  },
+};
+</script>
+
+<style></style>
