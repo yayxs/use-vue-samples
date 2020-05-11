65dd3718c646dc5b1bdbe2fbab7b1da6dd9fd137
yangwl <yangwl@zonekey.com.cn>
2020-05-11 18:59:23 +0800

    [form-validate]动态增减表单项

---
+++ form-validate/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <dynamic-validate-form></dynamic-validate-form>
+  </div>
+</template>
+
+<script>
+import DynamicValidateForm from '@/components/DynamicValidateForm.vue'
+export default {
+  name: 'app',
+  components: {
+    DynamicValidateForm
+  }
+}
+</script>
+
+<style>
+
+</style>
